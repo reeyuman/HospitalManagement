705ae7278b80782ae4247b1170cd98d8f8af1b85
reeyuman <reeyuman.ve@gmail.com>
2024-10-09 00:47:18 +0300

    fixError

--- HospitalManagement/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
